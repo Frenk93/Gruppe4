fcd64e591155c80ff6bc666c623f88349d8f6d45
frenks <shabani1@web.de>
2024-03-12 11:29:40 +0100

    new task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+
+        String asciiArt1 = FigletFont.convertOneLine("hello world");
+        System.out.println(asciiArt1);
     }
 }
